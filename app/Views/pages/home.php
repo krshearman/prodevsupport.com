0d0a01a442bddf13c95b5e326088efd18cb06e6d
kendall <kendallshearman@gmail.com>
2024-08-17 16:35:12 -0700

    fixed css for home

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -3,7 +3,7 @@
 
     <!-- ======= About Section ======= -->
     <section id="about" class="about-mf sect-pt4 route myabout">
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-12">
                     <div class="box-shadow-full">
@@ -14,7 +14,7 @@
                                         Welcome
                                     </h3>
                                 </div>
-                                    <p class="lead minht">Thanks for visiting! This portfolio site showcases my latest
+                                    <p class="lead container-fluid">Thanks for visiting! This portfolio site showcases my latest
                                         experience, skills, and expertise. You can learn more about me in the <strong><a href="<?php echo base_url(); ?>about" target="_blank">About</a></strong>
                                         section and see samples of my code and documentation style/technical writing by
                                         clicking Portfolio in the horizontal menu above. The code for this project and some of my other projects is on <strong><a href="https://github.com/krshearman" target="_blank">Github <ion-icon name="logo-github"></ion-icon></a></strong>.
